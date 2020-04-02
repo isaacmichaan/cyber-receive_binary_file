a5ed13de64b621e86295b58574e713a923efc78b
Isaac Michaan <46087877+isaacmichaan@users.noreply.github.com>
2020-04-03 02:15:41 +0300

    Update receive_binary_file.py

--- receive_binary_file.py
+++ receive_binary_file.py
@@ -1,15 +1,28 @@
 from scapy.all import *
 
+src = '10.0.2.4'
+d = {} #dictionary
 
-# function which receives a binary file from Kali VM
 def handler(pkt):
-	try:
-		f = open("nc", "ab")
-		f.write(pkt['Raw'].load)
-		f.close()
-		if pkt['Raw'].load == b'Finished':
-			return True
-	except IndexError:
-		pass
+        try:
+                if pkt['Raw'] and pkt['IP'].src != src:
+                        #if pkt['Raw'].load == b'Finished':
+                                #return 1
 
-sniff(stop_filter=handler, filter="icmp")
+                        # save to dict
+                        if str(pkt['IP'].id) not in d:
+                                d[str(pkt['IP'].id)] = str(pkt['Raw'].load.decode())
+                                return
+
+                        # write to file
+                        try:
+                                f = open(d[str(pkt['IP'].id)], "ab")
+                                f.write(pkt['Raw'].load)
+                                f.close()
+                        except:
+                                return handler(pkt)
+
+        except IndexError:
+                pass
+
+sniff(prn=handler, filter="icmp")
